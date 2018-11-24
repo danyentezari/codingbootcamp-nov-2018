f1c7f9c1e2d60727c00be331b919a68cae369ed2
Dany Entezari <dany.entezari@gmail.com>
2018-11-24 15:06:02 +0400

    Changed keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    secret:process.env.SECRET
+    mongoURI: "mongodb://admin01:abc123@ds135196.mlab.com:35196/astrolabs",
+    secret: "asdasdasd23123"
 };
